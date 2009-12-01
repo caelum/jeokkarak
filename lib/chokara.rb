91986bbe6ce6d2f5f7d301e47d9b44941ee55ae2
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-12-01 21:43:00 -0200

    allows direct setting of attribute

--- lib/chokara.rb
+++ lib/chokara.rb
@@ -2,8 +2,24 @@ module Chokara
   module Base
     def from_hash(h)
       #      Hashi::CustomHash.new(h)
-      instance = new
-      instance
+      h = h.dup
+      links = nil
+      h.each do |key,value|
+        case value.class.to_s
+        when 'Array'
+          h[key].map! { |e| reflect_on_association(key.to_sym ).klass.from_hash e }
+        when /\AHash(WithIndifferentAccess)?\Z/
+          h[key] = reflect_on_association(key.to_sym ).klass.from_hash value
+        end
+      end
+      result = self.new
+      h.each do |key, value|
+        result.send("#{key}=",value) if result.respond_to?("#{key}=")
+      end
+#      if !(links.nil?) && self.include?(Restfulie::Client::Instance)
+#        result.add_transitions(links)
+#      end
+      result
     end
   end
 end
