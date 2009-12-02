8afded36302fc4f4e4aace7720212b35b4bf95d1
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-12-01 22:01:25 -0200

    attribution is fine

--- spec/chokara_spec.rb
+++ spec/chokara_spec.rb
@@ -11,40 +11,48 @@ describe Chokara::Base do
   context "when creating an object from a hash" do
     it "should create that class's object" do
       hash = {"name" => "guilherme silveira"}
-      Product.from_hash(hash).class.should eql(Product)
+      product = Product.from_hash(hash)
+      product.class.should eql(Product)
     end
     it "should allow direct attribute access" do
       hash = {"name" => "guilherme silveira"}
-      Product.from_hash(hash).name.should == ("guilherme silveira")
+      product = Product.from_hash(hash)
+      product.name.should == ("guilherme silveira")
     end
     it "should allow direct attribute attribution" do
       hash = {"name" => "guilherme silveira"}
-      Product.from_hash(hash).name = "donizetti"
-      Product.from_hash(hash).name.should == ("donizetti")
+      product = Product.from_hash(hash)
+      product.name = "donizetti"
+      product.name.should == ("donizetti")
     end
-    # it "should allow access to child element" do
-    #   hash = {"player" => {"name" => "guilherme silveira"}}
-    #   Product.from_hash(hash).player.name.should == ("guilherme silveira")
-    # end
-    # it "should allow access to boolean element" do
-    #   hash = {"player" => {"name" => "guilherme silveira", "valid" => true}}
-    #   Product.from_hash(hash).player.valid.should be_true
-    # end
-    # it "should allow access attribution to child element" do
-    #   hash = {"player" => {"name" => "guilherme silveira"}}
-    #   Product.from_hash(hash).player.name = "donizetti"
-    #   Product.from_hash(hash).player.name.should == ("donizetti")
-    # end
-    # it "should allow access to an array element" do
-    #   hash = {"player" => [{"name" => "guilherme silveira"}, {"name" => "caue guerra"}]}
-    #   Product.from_hash(hash).player[1].name.should == ("caue guerra")
-    # end
-    # it "should allow access attribution to an array element" do
-    #   hash = {"player" => [{"name" => "guilherme silveira"}, {"name" => "caue guerra"}]}
-    #   Product.from_hash(hash).player[1].name = "donizetti"
-    #   Product.from_hash(hash).player[1].name.should == ("donizetti")
-    # end
-    #     
+    it "should allow access to child element" do
+      hash = {"player" => {"name" => "guilherme silveira"}}
+      product = Product.from_hash(hash)
+      product.player.name.should == ("guilherme silveira")
+    end
+    it "should allow access to boolean element" do
+      hash = {"player" => {"name" => "guilherme silveira", "valid" => true}}
+      product = Product.from_hash(hash)
+      product.player.valid.should be_true
+    end
+    it "should allow access attribution to child element" do
+      hash = {"player" => {"name" => "guilherme silveira"}}
+      product = Product.from_hash(hash)
+      product.player.name = "donizetti"
+      product.player.name.should == ("donizetti")
+    end
+    it "should allow access to an array element" do
+      hash = {"player" => [{"name" => "guilherme silveira"}, {"name" => "caue guerra"}]}
+      product = Product.from_hash(hash)
+      product.player[1].name.should == ("caue guerra")
+    end
+    it "should allow access attribution to an array element" do
+      hash = {"player" => [{"name" => "guilherme silveira"}, {"name" => "caue guerra"}]}
+      product = Product.from_hash(hash)
+      product.player[1].name = "donizetti"
+      product.player[1].name.should == ("donizetti")
+    end
+        
   end
   
 end
